b6a0d1c51a49166b1cfec312c15f2ee701821d80
Keks <keks@htmlacademy.ru>
2024-03-28 11:56:56 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/render-photos.js
@@ -0,0 +1,19 @@
+const renderPhotos = (photosData) => {
+
+  const photosEl = document.querySelector('.pictures');
+  const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  const photoFragment = document.createDocumentFragment();
+
+  photosData.forEach(({url, description, likes, comments}) => {
+    const photoEl = photoTemplate.cloneNode(true);
+    const imageEl = photoEl.querySelector('img');
+    imageEl.src = url;
+    imageEl.alt = description;
+    photoEl.querySelector('.picture__likes').textContent = likes;
+    photoEl.querySelector('.picture__comments').textContent = comments.length;
+    photoFragment.append(photoEl);
+  });
+  photosEl.append(photoFragment);
+};
+
+export {renderPhotos};
